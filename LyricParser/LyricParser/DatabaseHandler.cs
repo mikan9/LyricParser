05c823bbe3cefae1e9ee86bdecd1d73e8aeacef6
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2019-10-18 05:28:50 +0200

    Updated .gitignore

--- LyricParser/LyricParser/DatabaseHandler.cs
+++ LyricParser/LyricParser/DatabaseHandler.cs
@@ -34,7 +34,7 @@ namespace LyricParser
                         }
                         catch(SQLiteException e)
                         {
-                            transaction.Rollback();
+                            Trace.WriteLine(e.Message);
                         }
                     }
 
