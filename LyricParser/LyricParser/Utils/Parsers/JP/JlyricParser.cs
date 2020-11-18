e6f43f2b64b6d38791ba172c08988379e12f2ec6
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 15:19:40 +0100

    Fix parser not finding lyrics

--- LyricParser/LyricParser/Utils/Parsers/JP/JlyricParser.cs
+++ LyricParser/LyricParser/Utils/Parsers/JP/JlyricParser.cs
@@ -3,6 +3,8 @@ using LyricParser.Extensions;
 using LyricParser.Common;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq;
 
 namespace LyricParser.Utils.Parsers.JP
 {
@@ -16,9 +18,35 @@ namespace LyricParser.Utils.Parsers.JP
         public override async Task<string> ParseHtml(string artist, string title, string optional = "")
         {
             string html = await GetHtml(BaseUrl + title.Replace(" ", "+") + "&ct=1&ka=" + artist);
-            if (string.IsNullOrEmpty(html)) return null;
-
             HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            bool foundMatch = false;
+            var mnb = doc.DocumentNode.SelectSingleNode("//*[contains(@id,'mnb')]");
+            var bdy = mnb.SelectNodes(".//*[contains(@class, 'bdy')]");
+
+            for (int i = 0; i < bdy.ToArray().Length; ++i)
+            {
+                string _title = "";
+                string _artist = "";
+
+                var mid = bdy.ElementAt(i).SelectSingleNode(".//*[contains(@class,'mid')]");
+                var sml = bdy.ElementAt(i).SelectSingleNode(".//*[contains(@class,'sml')]");
+
+                _title = mid.FirstChild.InnerText.ToLower().TrimStart().TrimEnd();
+                _artist = sml.ChildNodes.ElementAt(1).InnerText.ToLower().TrimStart().TrimEnd();
+
+                if (_title == title.ToLower() && _artist.Contains(artist.ToLower()))
+                {
+                    foundMatch = true;
+                    html = await GetHtml(mid.FirstChild.GetAttributeValue("href", "null"));
+                    break;
+                }
+            }
+
+            if (!foundMatch || string.IsNullOrEmpty(html)) return null;
+
+            doc = new HtmlDocument();
             doc.LoadHtml(html);
 
             return CleanUp(doc.DocumentNode.SelectSingleNode("//*[contains(@id,'Lyric')]").InnerHtml);
