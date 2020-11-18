296ff4979e6f30141b5483ec912a8db08265196a
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 14:48:34 +0100

    Create model for lyrics

---
+++ LyricParser/LyricParser/Models/Lyrics.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+
+namespace LyricParser.Models
+{
+    public class Lyrics
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [Indexed(Name = "LyricsID", Order = 1, Unique = true)]
+        public string Artist { get; set; }
+        [Indexed(Name = "LyricsID", Order = 2, Unique = true)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public static Lyrics Empty()
+        {
+            return new Lyrics()
+            {
+                ID = -1,
+                Artist = "",
+                Title = "",
+                Content = ""
+            }; 
+        }
+    }
+}
