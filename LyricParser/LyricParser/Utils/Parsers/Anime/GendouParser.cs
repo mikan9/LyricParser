9d1b1e4692deb312967cd06b324ea25f2fad4004
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 14:50:52 +0100

    Add parser for lyrics from Gendou

---
+++ LyricParser/LyricParser/Utils/Parsers/Anime/GendouParser.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using LyricParser.Common;
+using System.Threading.Tasks;
+
+namespace LyricParser.Utils.Parsers.Anime
+{
+    public class GendouParser : HtmlParser
+    {
+        public GendouParser()
+        {
+            BaseUrl = Constants.GENDOU_URL;
+        }
+
+        public override async Task<string> ParseHtml(string artist, string title, string optional = "")
+        {
+            string html = await GetHtml(BaseUrl + artist.Replace(" ", "+") + "+" + title.Replace(" ", "+") + optional);
+            if (string.IsNullOrEmpty(html)) return null;
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            return CleanUp(doc.DocumentNode.SelectSingleNode("//*[contains(@id,'content_1')]").InnerHtml);
+        }
+    }
+}
