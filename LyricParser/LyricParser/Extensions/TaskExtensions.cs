7a0f3c518ffac98597b3a506aa7ac9ac58a225f4
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 14:48:17 +0100

    Implement simple async fire-and-forget method

---
+++ LyricParser/LyricParser/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LyricParser.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async void SafeFireAndForget(this Task task,
+            bool returnToCallingContext,
+            Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+            catch(Exception ex) when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+}
