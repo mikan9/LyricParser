d9c688ab17ca0b56d30edbb9f4b581a515d38cba
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 14:51:04 +0100

    Add parser for lyrics from J-Lyric

---
+++ LyricParser/LyricParser/Utils/Parsers/JP/JlyricParser.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using LyricParser.Extensions;
+using LyricParser.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LyricParser.Utils.Parsers.JP
+{
+    public class JlyricParser : HtmlParser
+    {
+        public JlyricParser()
+        {
+            BaseUrl = Constants.JLYRIC_URL;
+        }
+
+        public override async Task<string> ParseHtml(string artist, string title, string optional = "")
+        {
+            string html = await GetHtml(BaseUrl + title.Replace(" ", "+") + "&ct=1&ka=" + artist);
+            if (string.IsNullOrEmpty(html)) return null;
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            return CleanUp(doc.DocumentNode.SelectSingleNode("//*[contains(@id,'Lyric')]").InnerHtml);
+        }
+    }
+}
