f3a092925a9f34c3244c673c1b5c60877cbe83b7
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-18 14:45:09 +0100

    Move global enums to single file

---
+++ LyricParser/LyricParser/Common/Enumerators.cs
@@ -0,0 +1,33 @@
+﻿namespace LyricParser.Common
+{
+    public enum Category
+    {
+        None = -1,
+        Anime = 0,
+        Touhou = 1,
+        Western = 2,
+        JP = 3,
+        Other = 4
+    }
+
+    // Enum for the supported media players
+    public enum Player
+    {
+        Winamp = 0,
+        Spotify = 1,
+        Youtube = 2,
+        GooglePlayMusic = 3
+    }
+
+    public enum Status
+    {
+        Done,
+        Searching,
+        Parsing,
+        Failed,
+        Standby,
+        SaveSuccessFul,
+        SaveFailed,
+        Saving
+    }
+}
