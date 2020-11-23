262739d356c5d09e7aff42d944c11be0538398a5
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-11-23 15:25:28 +0100

    Add event publisher for currently playing song

---
+++ LyricParser/LyricParser/Events/CurrentlyPlayingUpdatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Events;
+using Windows.Media.Control;
+
+namespace LyricParser.Events
+{
+    public class CurrentlyPlayingUpdatedEvent : PubSubEvent<GlobalSystemMediaTransportControlsSession>
+    {
+    }
+}
