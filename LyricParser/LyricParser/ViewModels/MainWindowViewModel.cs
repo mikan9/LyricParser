8eb7e3633d8671520435d0acda7a23b1e0078803
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-12-21 16:33:51 +0100

    Add trim to artist and title when not auto searching

--- LyricParser/LyricParser/ViewModels/MainWindowViewModel.cs
+++ LyricParser/LyricParser/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using LyricParser.Common;
+using LyricParser.Extensions;
 using LyricParser.Models;
 using LyricParser.Resources;
 using LyricParser.Services.Interfaces;
@@ -583,11 +584,10 @@ namespace LyricParser.ViewModels
             else
             {
                 string[] artistTitle = Song.FromString(SongEntry);
-                await GetLyrics(artistTitle[0], artistTitle[1]);
+                await GetLyrics(artistTitle[0].Trim(), artistTitle[1].Trim());
             }
         }
 
-        // Update last song and search history then begin fetching lyrics for the current song
         async Task GetLyrics(string artist, string title)
         {
             CleanUp();
@@ -595,7 +595,9 @@ namespace LyricParser.ViewModels
             bool success = true;
 
             retries = MAX_RETRIES;
-            var lyrics = await App.Database.GetLyricsAsync(artist.ToLower(), title.ToLower());
+
+            Lyrics lyrics = AutoSearchChecked ? await App.Database.GetLyricsAsync(artist.ToLower(), title.ToLower()) : null;
+
             if(lyrics == null)
             {
                 success = await AddLyrics(artist, title);
@@ -621,8 +623,8 @@ namespace LyricParser.ViewModels
 
             SetStatus(Status.Parsing);
 
-            string _title = title.ToLower().Trim().Normalize();
-            string _artist = artist.ToLower().Trim().Normalize();
+            string _title = title.ToLower().Trim().RemoveDiacritics().Normalize();
+            string _artist = artist.ToLower().Trim().RemoveDiacritics().Normalize();
 
             switch (songCategory)
             {
@@ -637,6 +639,7 @@ namespace LyricParser.ViewModels
                     break;
                 case Category.Western:
                     content = await new MetrolyricsParser().ParseHtml(_artist, _title);
+                    if (content == null) content = await new MusixmatchParser().ParseHtml(_artist, _title);
                     break;
                 case Category.Other:
                     content = await new AtwikiParser().ParseHtml(_artist, _title);
@@ -659,8 +662,8 @@ namespace LyricParser.ViewModels
         {
             await App.Database.SaveLyricsAsync(new Lyrics()
             {
-                Artist = artist.ToLower(),
-                Title = title.ToLower(),
+                Artist = artist.ToLower().Trim(),
+                Title = title.ToLower().Trim(),
                 Content = content
             });
         }
