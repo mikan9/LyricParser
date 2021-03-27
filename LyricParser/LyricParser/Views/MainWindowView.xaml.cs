346780ac1b1f766a9524622112388be2104c4cd8
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2021-03-27 20:10:32 +0100

    Remove unnecessary code

--- LyricParser/LyricParser/Views/MainWindowView.xaml.cs
+++ LyricParser/LyricParser/Views/MainWindowView.xaml.cs
@@ -8,12 +8,6 @@ namespace LyricParser
         public MainWindowView()
         {
             InitializeComponent();
-
-            Top = Properties.Settings.Default.Top >= 0 ? Properties.Settings.Default.Top : 0;
-            Left = Properties.Settings.Default.Left >= 0 ? Properties.Settings.Default.Left : 0;
-            Height = Properties.Settings.Default.Height;
-            Width = Properties.Settings.Default.Width;
-            if (Properties.Settings.Default.Maximized) WindowState = WindowState.Maximized;
         }
     }
 }
